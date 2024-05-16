f2023eaed2fd2aff6b23fc9f5169850591ed5f92
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-05-16 17:04:09 +0100

    chore: stop

--- src/handlers/user-start-stop.ts
+++ src/handlers/user-start-stop.ts
@@ -12,15 +12,14 @@ import { getMultiplierInfoToDisplay } from "./shared/get-multiplier-info";
 import { closePullRequestForAnIssue } from "./shared/assign";
 
 export async function userStartStop(context: Context): Promise<{ output: string | null }> {
-  const { payload, logger } = context;
+  const { payload, logger, config } = context;
   const { issue, comment, sender, repository } = JSON.parse(payload) as Context<"issue_comment.created">["payload"];
   const directive = comment.body.split(" ")[0].replace("/", "");
   const { disabledCommands } = config;
   const isCommandDisabled = disabledCommands.some((command: string) => command === directive);
 
   if (isCommandDisabled) {
-    await addCommentToIssue(context, `The command '/${directive}' is disabled in this repository.`);
-    return { output: null };
+    throw logger.error(`The '/${directive}' command is disabled for this repository.`);
   }
 
   if (directive === "stop") {
